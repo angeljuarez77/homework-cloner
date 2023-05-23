b2ccfa5bf55de7405b99dda4fbe7e015c9e4d9c2
Angel Juarez <angeljuarez729@gmail.com>
2023-05-23 00:28:28 -0400

    idk

--- src/HomeworkCloner.ts
+++ src/HomeworkCloner.ts
@@ -10,46 +10,41 @@ const users_names = {
 };
 
 class HomeworkCloner {
-  github_url: string = 'https://git.generalassemb.ly/api/v3';
+  github_url = 'https://git.generalassemb.ly/api/v3';
   access_token: string = process.env.GITHUB_PAT;
 
   async run(): Promise<void> {
-    await this.promptUser();
+    await this.promptUser('synchrony', '4', 'hogwarts');
   }
 
-  async promptUser() {
-    let cohort: string;
-    let unit: string;
-    let assignment: string;
+  async promptUser(cohortParam: string, unitParam: string, assignmentParam: string) {
+    let cohort: string = cohortParam;
+    let unit: string = unitParam;
+    let assignment: string = assignmentParam;
     try {
-      cohort = await question(`What ${chalk.blue('cohorts')} assignment are your grading? `);
+      cohort ||= await question(`What ${chalk.blue('cohorts')} assignment are your grading? `);
       if(cohort === '') throw new Error('Invalid input for cohort.');
 
-      unit = await question(`Which ${chalk.blue('unit')} are you in currently? (Just a numerical value) `);
-      if(!unit || !Boolean(parseInt(unit))) throw new Error('Invalid input for unit.');
+      unit ||= await question(`Which ${chalk.blue('unit')} are you in currently? (Just a numerical value) `);
+      if(!unit || isNaN(parseInt(unit))) throw new Error('Invalid input for unit.');
 
-      assignment = await question(`Which ${chalk.blue('assignment')} do you want to grade? `);
+      assignment ||= await question(`Which ${chalk.blue('assignment')} do you want to grade? `);
       if(!assignment || assignment === '') throw new Error('Invalid input for assignment.');
     } catch(e) {
       console.error('Something went wrong with gathering user prompts: ', e.message);
     }
-    console.log(cohort, 'unit' + unit, assignment); 
-    // this.clonePRs(cohort, 'unit' + unit, assignment);
+    this.clonePRs(cohort, 'unit' + unit, assignment);
   }
-
-  // async dependencyInstall() {
-
-  // }
 
   async clonePRs(cohort: string, unit: string, assignment: string): Promise<void> {
     const pullRequests: PullRequest[] = await this.getPRs(cohort, assignment);
 
     pullRequests.forEach(async (pr: PullRequest): Promise<void> => {
       try {
-        const cloneUrl: string = `https://git.generalassemb.ly/${pr.user.login}/${assignment}.git`
+        const cloneUrl = `https://git.generalassemb.ly/${pr.user.login}/${assignment}.git`;
         const studentActualName: string = users_names[pr.user.login];
-        console.log(`${chalk.blue('Cloning down')} ${chalk.red(studentActualName)}`)
-        await quiet($`git clone ${cloneUrl} ~/Repositories/general_assembly/synchrony/grading/${unit}/${assignment}/${studentActualName} -q`);
+        console.log(`${chalk.blue('Cloning down')} ${chalk.red(studentActualName)}`);
+        await $`git clone ${cloneUrl} ~/Repositories/general_assembly/${cohort}/grading/${unit}/${assignment}/${studentActualName}`;
       } catch(e) {
         console.error('Something went wrong with the cloning', e.message);
       }
@@ -57,16 +52,24 @@ class HomeworkCloner {
   }
 
   async getPRs(cohort: string, assignment: string): Promise<PullRequest[]> {
-    const res = await fetch(`${this.github_url}/repos/${cohort}/${assignment}/pulls`, {
-      method: 'GET',
-      headers: {
-        Authorization: `token ${this.access_token}`
+    try {
+      const res = await fetch(`${this.github_url}/repos/${cohort}/${assignment}/pulls`, {
+        method: 'GET',
+        headers: {
+          Authorization: `token ${this.access_token}`
+        }
+      });
+
+      const pullRequests: PullRequest[] = JSON.parse(await res.text());
+
+      if(pullRequests.length === 0) {
+        throw new Error('There were no pull requests. Are you sure sumbissions were not submitted through Issue Tickets?');
       }
-    });
 
-    const pullRequests: PullRequest[] = JSON.parse(await res.text());
-
-    return pullRequests;
+      return pullRequests;
+    } catch(e) {
+      console.error(e);
+    }
   }
 }
 
